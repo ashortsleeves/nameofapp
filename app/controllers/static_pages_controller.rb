2b91bc332e675030bedda51736f2c3e17a396bad
ashortsleeves <ashortsleeves@gmail.com>
2016-08-19 12:36:17 -0400

    4.5 Carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
+	def index
+  		@products = Product.limit(4)
+	end
 
-  def landing_page
-	@featured_product = Product.first
+	def landing_page
+  		@products = Product.limit(4)
 	end
 
 end
