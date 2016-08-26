81c2a09a18d97ed70b7b1554632841ff201b1b5e
ashortsleeves <ashortsleeves@gmail.com>
2016-08-26 14:09:00 -0400

    5.1 routes and redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to "/static_pages/landing_page", notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -36,6 +36,8 @@ class ProductsController < ApplicationController
       end
     end
   end
+# redirect_to @product
+
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
