06322bc176f76ebdec526a5d89fc79d85965f5b8
ashortsleeves <ashortsleeves@gmail.com>
2016-09-06 17:03:14 -0400

    4.6 made fixes from Mentor Video

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
-  root 'products#index'
+  root 'static_pages#landing_page'
 
   #root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
