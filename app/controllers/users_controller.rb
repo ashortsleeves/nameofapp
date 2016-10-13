10424cacce9f8bb550650b0b99d1dceb358dfa46
ashortsleeves <ashortsleeves@gmail.com>
2016-10-13 12:08:48 -0400

    5.10b finalized admin and cancan permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,7 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
@@ -73,3 +74,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:first_name, :last_name)
     end
 end
+
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-  end
 
+    if user.admin?
+      can :manage, :all
+
+    else
+      can :read, :all
+      cannot [:update, :destroy], [Product, User, Comment]
+      cannot [:index, :show, :create], User
+      cannot :create, Product
+    end
+  end
 end
