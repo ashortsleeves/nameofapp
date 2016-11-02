422fbcb2aa3391cbf50361694beeeeb6c14f8ec4
Adam <ashortsleeves@gmail.com>
2016-11-02 18:47:20 +0000

    added stripe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013124025) do
+ActiveRecord::Schema.define(version: 20161102174918) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20161013124025) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
