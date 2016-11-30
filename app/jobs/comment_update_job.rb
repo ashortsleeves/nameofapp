7e967b3f38004ab81778c3125bc00a9cc6226414
Adam <ashortsleeves@gmail.com>
2016-11-30 11:48:55 +0000

    6.14o added render comment to comment_update_job.rb

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -4,4 +4,8 @@ class CommentUpdateJob < ApplicationJob
   def perform(comment, current_user)
     ProductChannel.broadcast_to(@product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user }), average_rating: @product.average_rating)
   end
+  
+  def render_comment(comment, current_user)
+    CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
+  end
 end
