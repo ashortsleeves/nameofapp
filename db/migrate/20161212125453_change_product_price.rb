389baf82d069a1751962d7c3106a649299ba17c1
Adam <ashortsleeves@gmail.com>
2016-12-12 13:12:14 +0000

    7.0.3 added order orderstatus, orderItem and changed product price

---
+++ db/migrate/20161212125453_change_product_price.rb
@@ -0,0 +1,5 @@
+class ChangeProductPrice < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :products, :price
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212124443) do
+ActiveRecord::Schema.define(version: 20161212125848) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -28,21 +28,44 @@ ActiveRecord::Schema.define(version: 20161212124443) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.decimal  "unit_price",  precision: 12, scale: 3
+    t.integer  "quantity"
+    t.decimal  "total_price", precision: 12, scale: 3
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
   create_table "order_statuses", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.decimal  "subtotal",        precision: 12, scale: 3
+    t.decimal  "tax",             precision: 12, scale: 3
+    t.decimal  "shipping",        precision: 12, scale: 3
+    t.decimal  "total",           precision: 12, scale: 3
+    t.integer  "order_status_id"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["order_status_id"], name: "index_orders_on_order_status_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "color"
-    t.integer  "price"
     t.text     "specs"
+    t.decimal  "price",       precision: 12, scale: 3
   end
 
   create_table "users", force: :cascade do |t|
